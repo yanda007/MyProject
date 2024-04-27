c7ea1a7dd2f34f435a0171d20070dc3148088bb9
yanda <yanda_newcom@163.com>
2024-04-28 00:06:39 +0800

    init

---
+++ magicians.py
@@ -0,0 +1,7 @@
+magicians=['alice','david','carolina']
+for magician in magicians:
+    print(f"{magician.title()},that was a great trick!")
+    print(f"I can't wati to see you next tric,{magician.title()}.\n")
+
+print("Bye Bye!!")
+
